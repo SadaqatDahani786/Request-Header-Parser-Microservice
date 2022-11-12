983958ea5103a79d83b428c2ed0814ac5319a6f9
Sadaqat Dahani <sadaqat.dahani2013@gmail.com>
2022-11-13 01:16:05 +0500

    Added view index.html and route whoami.

--- app.js
+++ app.js
@@ -12,14 +12,45 @@ const cors = require("cors");
  ** **
  */
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 /*
  ** **
  ** ** ** MIDDLEWARES
  ** **
  */
+app.use(express.static("views"));
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+/*
+ ** **
+ ** ** ** ROUTES
+ ** **
+ */
+/**
+ ** =============================================
+ ** Route [/]
+ ** =============================================
+ */
+app.get("/", (req, res) => {
+  //Send a index.html as response
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+/**
+ ** =============================================
+ ** Route [/whoami]
+ ** =============================================
+ */
+app.get("/api/whoami", (req, res) => {
+  //1) Get client data from req object
+  const ipaddress = req.ip;
+  const software = req.header("User-Agent");
+  const language = req.header("Accept-Language");
+
+  //2) Send a response
+  res.json({ ipaddress, language, software });
+});
 
 /*
  ** **
